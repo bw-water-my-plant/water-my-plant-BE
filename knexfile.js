bd8b1176fddd1e22d1776aea420432570746045f
motorcitycode <detropia.com@gmail.com>
2019-06-24 15:36:33 -0400

    fixed production database directory location

--- knexfile.js
+++ knexfile.js
@@ -23,24 +23,24 @@ module.exports = {
   testing: {
     client: 'sqlite3',
     connection: {
-      filename: './data/test.db3',
+      filename: './database/test.db3',
     },
     useNullAsDefault: true,
     migrations: {
-      directory: './data/migrations',
+      directory: './database/migrations',
     },
     seeds: {
-      directory: './data/seeds',
+      directory: './database/seeds',
     },
   },
   production: {
     client: 'pg',
     connection: dbConnnection,
     migrations: {
-      directory: './data/migrations',
+      directory: './database/migrations',
     },
     seeds: {
-      directory: './data/seeds',
+      directory: './database/seeds',
     },
   },
 };
